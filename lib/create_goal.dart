2f06b0b224267f946a446639f025f3a14b89f5fc
Alexander Topchiy <topchiyau@gmail.com>
2019-07-01 19:17:00 +0300

    Implement Create goal feature with goal name and create goal button
    - Add and implement Goal name text field
    - Add and implement Create goal button

--- lib/create_goal.dart
+++ lib/create_goal.dart
@@ -1,18 +1,77 @@
 import 'package:flutter/material.dart';
 
+import 'package:my_goals/data/model/goal.dart';
+
 class CreateGoal extends StatefulWidget {
   @override
   _CreateGoalState createState() => _CreateGoalState();
 }
 
 class _CreateGoalState extends State<CreateGoal> {
+  final _goalNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _goalNameController.dispose();
+    super.dispose();
+  }
+
+  void _createGoal() {
+    String _goalName = _goalNameController.text;
+    if (_goalName.isNotEmpty) {
+      Navigator.of(context).pop(
+          Goal(_goalName, 10)
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(
-          'Создать цель'
+        title: Text('Создать цель'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextFormField(
+                controller: _goalNameController,
+                decoration: InputDecoration(
+                  labelText: 'Название твоей цели',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Введите название цели';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.sentences,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: ButtonTheme(
+                minWidth: double.infinity,
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () => _createGoal(),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text(
+                    'СОЗДАТЬ',
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/my_goals.dart
+++ lib/my_goals.dart
@@ -3,6 +3,10 @@ import 'dart:async' show Future;
 import 'package:flutter/material.dart';
 
 import 'create_goal.dart';
+import 'package:my_goals/data/goals_repository.dart';
+import 'package:my_goals/data/model/goal.dart';
+
+final _goalsRepository = GoalsRepository();
 
 class MyGoals extends StatefulWidget {
   @override
@@ -12,8 +16,17 @@ class MyGoals extends StatefulWidget {
 class _MyGoalsState extends State<MyGoals> {
   bool _isListEmpty = true;
 
+  Widget _initGoalsListView() {
+    return ListView.builder(
+      itemCount: _goalsRepository.getGoalsList().length,
+      itemBuilder: (context, position) => ListTile(
+        title: Text(_goalsRepository.getGoalsList()[position].goalName),
+      ),
+    );
+  }
+
   Future _createNewGoal() async {
-    Map goal =  await Navigator.of(context).push(new MaterialPageRoute(
+    Goal goal =  await Navigator.of(context).push(new MaterialPageRoute(
       builder: (BuildContext context) {
         return CreateGoal();
       },
@@ -22,6 +35,7 @@ class _MyGoalsState extends State<MyGoals> {
     if (goal != null) {
       setState(() {
         _isListEmpty = false;
+        _goalsRepository.addNewGoalToList(goal.goalName, goal.goalDate);
       });
     }
   }
@@ -35,19 +49,20 @@ class _MyGoalsState extends State<MyGoals> {
           'Мои цели',
         ),
       ),
-      body: Center(
-        child: Visibility(
-          child: Text(
-            'Тут пусто :с',
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Visibility(
+              child: Text('Тут пусто :с'),
+              visible: _isListEmpty,
+            ),
           ),
-          visible: _isListEmpty,
-        ),
+          _initGoalsListView(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _createNewGoal();
-        },
-        tooltip: 'Create goal',
+        onPressed: () => _createNewGoal(),
+        tooltip: 'Создать цель',
         child: Icon(Icons.add),
       ),
     );
